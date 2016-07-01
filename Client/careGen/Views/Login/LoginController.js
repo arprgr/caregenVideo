62328877dfdff3a2b6897c9527749a1be43cccfc
arprgr <gregory.pillai@gmail.com>
2016-07-01 12:28:28 +0530

    latest version

--- Client/careGen/Views/Login/LoginController.js
+++ Client/careGen/Views/Login/LoginController.js
@@ -11,41 +11,29 @@ angular.module('Login', ['ngDialog','Authentication'])
                     $scope.dataLoading = true;
 
                     AuthenticationService.Login($scope.formData, function(response) {
-                        if(response.status > 200 ){
-                            $scope.error = response.message;
+                        if(response.data.name == null){
+
+                            $scope.error = 'Invalid Password';
                             $scope.dataLoading = false;
 
                         } else {
                             AuthenticationService.SetCredentials($scope.formData.email, $scope.formData.password);
 
-                            ngDialog.close();
+                            $rootScope.userName = response.data.name;
+                            ngDialog.close( {
+                                scope: $scope }
+                            );
                             $location.path('/main');
                         }
                     });
 
                 };
+
+                $scope.clickToForgetPassword = function(){
+                    AuthenticationService.SetCredentials('dummy', 'dummy');
+                    ngDialog.close();
+                    $location.path('/forgetPassword');
+                }
             }]);
 
 
-/*angular.module('Authentication', ['ngDialog'])
-
-    .controller('LoginController',
-        ['$scope', '$rootScope', '$location', 'AuthenticationService',
-            function ($scope, $rootScope, $location, AuthenticationService) {
-                // reset login status
-                AuthenticationService.ClearCredentials();
-
-                $scope.login = function () {
-                    $scope.dataLoading = true;
-                    AuthenticationService.Login($scope.username, $scope.password, function(response) {
-                        if(response.success) {
-                            AuthenticationService.SetCredentials($scope.username, $scope.password);
-                            ngDialog.close();
-                            $location.path('/main');
-                        } else {
-                            $scope.error = response.message;
-                            $scope.dataLoading = false;
-                        }
-                    });
-                };
-            }]); */
