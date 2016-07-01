77e93413fc9722503cad1c2ada13b3f4ecab3635
arprgr <gregory.pillai@gmail.com>
2016-07-01 18:03:36 +0530

    fixed defect for repeating entries

--- routes/addNewUser.js
+++ routes/addNewUser.js
@@ -4,13 +4,28 @@ var models = require('../server/models/index');
 
 /* add new users to the user table */
 router.post('/', function(req, res) {
-models.Users.create({
-        name: req.body.name,
-        emailid: req.body.email,
-        password: req.body.password
+
+    models.Users.find({
+        where: {
+            emailid: req.body.email
+        }
     }).then(function(Users) {
-        res.json(Users);
-    });
+        if (!Users) {
+            models.Users.create({
+                name: req.body.name,
+                emailid: req.body.email,
+                password: req.body.password
+            }).then(function (Users) {
+                res.json(Users);
+            });
+        }
+        else {
+            console.log("user already registered");
+            res.status(230).send({ error: "user already registered" });
+
+        }
+    })
+
 });
 
 module.exports = router;
