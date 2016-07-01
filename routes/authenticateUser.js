35fef72f38f304e40471447dc72679ddace6a8bf
arprgr <gregory.pillai@gmail.com>
2016-07-01 12:20:32 +0530

    fixing pwd mismatch defect

--- routes/authenticateUser.js
+++ routes/authenticateUser.js
@@ -7,22 +7,22 @@ router.post('/', function(req, res) {
     models.Users.find({
         where: {
             emailid: req.body.email
+
         }
     }).then(function(Users) {
-        console.log('this is what I found!!');
-        console.log(Users.emailid);
-        console.log(Users.password);
 
         if (Users.password != req.body.password) {
 
            console.log('passwords do not match!!');
-           res.status(500).send({ error: "boo:(" });
+ //          res.status(500).send({ error: "Invalid Password" });
+            res.json({status: 500});
         }
         else {
-        console.log('passwords match!!');
+
         return (res.json(Users));
         }
     });
 });
 
 module.exports = router;
+
