3920285c19269a4d120f9a395a89d9705e4586f0
arprgr <gregory.pillai@gmail.com>
2016-07-13 23:11:30 +0530

    for login changes

--- Client/careGen/Views/Login/LoginController.js
+++ Client/careGen/Views/Login/LoginController.js
@@ -3,8 +3,8 @@
 angular.module('Login', ['ngDialog','Authentication'])
 
     .controller('LoginController',
-        ['$scope', '$location', 'ngDialog', '$rootScope', 'AuthenticationService',
-            function ($scope, $location, ngDialog, $rootScope, AuthenticationService) {
+        ['$scope', '$location', 'ngDialog', '$rootScope', 'AuthenticationService', 'SharedData',
+            function ($scope, $location, ngDialog, $rootScope, AuthenticationService, SharedData) {
 
                 $scope.login = function(){
 
@@ -17,12 +17,52 @@ angular.module('Login', ['ngDialog','Authentication'])
                             $scope.dataLoading = false;
 
                         } else {
-                            AuthenticationService.SetCredentials(response.data.email, response.data.password, response.data.name);
-                            
+
                             $rootScope.userName = response.data.name;
+                            
+                            SharedData.setValue(response.data.emailid);
+                            $scope.formData.senderEmailid = response.data.emailid;
+                            AuthenticationService.getSentInvitations($scope.formData, function(response) {
+
+                                if(response.data.length == 0){
+                                    $rootScope.noInvitations = 'No invitations.';
+                                }else {
+                                    $rootScope.noInvitations = '';
+                                }
+                                console.log( $rootScope.noInvitations );
+                                $rootScope.sentInvitationID = response.data;
+
+                             });
+
+                            AuthenticationService.getReceivedInvitations($scope.formData, function(response) {
+
+                                if(response.data.length == 0){
+                                    if($rootScope.noInvitations !== ''){
+                                    $rootScope.noInvitations = 'No invitations.'; }
+                                }else{
+                                    $rootScope.noInvitations = '';
+                                }
+                                console.log( $rootScope.noInvitations );
+                                $rootScope.receivedInvitationID = response.data;
+                            });
+
+
+                            AuthenticationService.getConnections($scope.formData, function(response) {
+
+                              if(response.data.length == 0){
+                                  $rootScope.noConnections = 'You have no connections yet.';
+                               } else {
+                                $rootScope.noConnections = '';
+                                $rootScope.connectionsID = response.data;
+                               }
+                            });
+
+                            AuthenticationService.SetCredentials(response.data.email, response.data.password, response.data.name);
+
                             ngDialog.close( {
                                 scope: $scope }
                             );
+                            
                             $location.path('/main');
                         }
                     });
