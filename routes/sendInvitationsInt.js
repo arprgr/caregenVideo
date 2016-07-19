1997192afbe456569c6e7381e6045095d59e7a68
arprgr <gregory.pillai@gmail.com>
2016-07-19 18:29:58 +0530

    reverting changes

--- routes/sendInvitationsInt.js
+++ routes/sendInvitationsInt.js
@@ -24,45 +24,6 @@ console.log(nconf.get('emailPass'));
 
 router.post('/', function(req, res) {
 
-  models.Connections.find({ attributes: ['id'] ,
-        where: {
-            primaryEmailid : req.body.senderEmailid,
-            connectedToEmailid : req.body.receiverEmailid
-        }
-    }).then(function(Connections) {   
-        if (Connections != null ) {
-        console.log('no need to go further connection already exists');
-        
-        res.status(230).end();  return;
-        }
-    });
-  
-
-    models.Invitations.find({ attributes: ['id'] ,
-        where: {
-            senderEmailid : req.body.senderEmailid,
-            receiverEmailid : req.body.receiverEmailid
-        }
-    }).then(function(Invitations) {   
-        if (Invitations != null) {
-        console.log('you have already invited this user');
-        
-        res.status(240).end();  return;
-        }
-    });
-
-  models.Invitations.find({ attributes: ['id'] ,
-        where: {
-            senderEmailid : req.body.receiverEmailid,
-            receiverEmailid : req.body.senderEmailid
-        }
-    }).then(function(Invitations) {   
-        if (Invitations != null) {
-        console.log('Outstanding invitation already exists');
-        
-        res.status(250).end();  return;
-        }
-    }); 
     
   
  var transporter = nodemailer.createTransport({
@@ -73,7 +34,7 @@ router.post('/', function(req, res) {
         }
        });
 
-    var text = req.body.senderEmailid + ' wants to connect with you, please click on the below link to respond \n\n' + "http://localhost:3000/Index.html#/?origin=email&emailid=" + req.body.senderEmailid;
+    var text = req.body.senderEmailid + ' wants to connect with you, please click on the below link to respond \n\n' + "https://shielded-refuge-67132.herokuapp.com/Index.html#/?origin=email&emailid=" + req.body.senderEmailid;
 
     var mailOptions = {
     from: nconf.get('emailUser'), // sender address
