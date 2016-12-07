96b54011c1ea2b324547d9ab00c6999d252cdada
arprgr <gregory.pillai@gmail.com>
2016-12-07 11:32:52 +0530

    latest changes including invite rework

--- Client/careGen/Views/Home/HomeController.js
+++ Client/careGen/Views/Home/HomeController.js
@@ -3,8 +3,8 @@
 angular.module('Home', ['ngDialog','Authentication'])
     
     .controller('HomeController',
-        ['$scope', 'ngDialog', '$location', '$rootScope', 'AuthenticationService',
-            function ($scope, ngDialog, $location, $rootScope, AuthenticationService) {
+        ['$scope', 'ngDialog', '$location', '$rootScope','$cookieStore' ,'SharedData','AuthenticationService',
+            function ($scope, ngDialog, $location, $rootScope, $cookieStore,SharedData, AuthenticationService) {
 
                 $scope.formData= {
                     email: $rootScope.registerEmail
@@ -47,6 +47,8 @@ angular.module('Home', ['ngDialog','Authentication'])
                 
                 $scope.buttonClick = function() {
                     $scope.dataLoading = true;
+                    
+                    console.log("checking if user existis");
 
                     AuthenticationService.VerifyId($scope.formData, function(response) {
 
@@ -68,5 +70,68 @@ angular.module('Home', ['ngDialog','Authentication'])
 
          
                 }
+                
+             $scope.handleFBLogin = function() {
+                    
+                    console.log('Handling FB login in controller'); 
+                    FB.api('/me?fields=email,id,name,picture', function(response) {
+                     $scope.formData= {
+                        email: response.email,
+                        name: response.name,
+                        password: 'facebook',
+                        senderEmailid: response.email 
+                    };      
+                       AuthenticationService.addUser($scope.formData,function(response){
+                        if(response.status > 190 ){
+                            
+                            $rootScope.userName = $scope.formData.name;
+                            $rootScope.userEmailId = $scope.formData.email;
+                            $rootScope.registerEmail = $scope.formData.email;    
+                            
+                    AuthenticationService.SetCredentials( $scope.formData.email, 'facebook', $scope.formData.name);
+                            
+                        SharedData.setValue($scope.formData.email);
+                            
+                            $cookieStore.put('userEmailId', $rootScope.userEmailId);
+                             
+                            console.log('added user going to main' + $rootScope.userName);
+                            
+
+                            $cookieStore.put('userEmailId', $rootScope.userEmailId);
+                                        
+                            $cookieStore.put('registerEmail', $rootScope.registerEmail);
+
+                            
+                            $location.path('/main');     
+                    
+                        }
+                    });                
+      
+                    });    
+                    
+             }      
+                
+               $scope.handleInvite = function() {
+                    $scope.dataLoading = true;
+                    
+                    console.log("In Handle Invite" + $rootScope.registerEmail);
+
+                    AuthenticationService.VerifyId($scope.formData, function(response) {
+
+                        if(response.status > 200 ) {
+                               AuthenticationService.SetCredentials($scope.formData.email);
+                               console.log('user does not exist, going to registration page');
+                               window.location.href="http://localhost:3000/Index.html#/?origin=email&emailid=" + $rootScope.registerEmail;
+                        } else {
+                           window.location.href="http://localhost:3000/Index.html#/?user=existing&emailid=" + $rootScope.registerEmail;
+                           };
+
+                            $scope.dataLoading = false;
+                        });
+
+         
+                } 
+               
+            
             }]);
 
